f34636131bf7bbf28cf4832d4a28fc422e9637df
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-12-22 08:44:44 -0800

    Add dotnet build workflow (#16)
    * Add dotnet build workflow
    * Remove 'REINSTALLMODE' from installer as it is not recommended
    * Add alternative search path for 'PlexService' in project file
    * Minor improvements to setup script to output commands
    * Add 'DevSkim' build to 'CodeQL' workflow
    * Remove 'Prefer32Bit' and update formatting

--- build.bat
+++ build.bat
@@ -13,6 +13,9 @@ exit /b
 :$Main
 setlocal EnableExtensions
     cd /D "%~dp0"
+    call :Command git add .
+    call :Command git clean -xfd
     call :Command dotnet tool restore
-    call :Command dotnet build
+    call :Command dotnet build --runtime win-x64 --configuration Debug "PlexService/"
+    call :Command dotnet build --runtime win-x64 --configuration Debug "PlexServiceInstaller/"
 endlocal & exit /b %ERRORLEVEL%
